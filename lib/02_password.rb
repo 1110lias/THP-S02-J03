e90ed365043490b5988fdf4662ac648ae1e419d9
1110lias <elias-b@outlook.fr>
2024-04-03 23:42:40 +0200

    late modif pour compréhension

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,30 +1,43 @@
-def signup
-    puts "Bonjour, veuillez définir un mpd ici:"
-    print "> "
-    user_mdp = gets.chomp
+#méthode sign_up
+def sign_up
+    puts "définissez votre mot de passe top secret de la mort qui tue."
+    print ">"
+    user_password = gets.chomp
+    puts "définissez votre login top secret de la mort qui tue."
+    print ">"
+    user_login = gets.chomp
+    return user_password, user_login
 end
 
-def login (user_mdp)
-    puts "Pour vous loguer, veuillez entrer le bon mdp"
-    print "> "
-    user_tentative = gets.chomp
-    while user_tentative != user_mdp
-        puts "Faux, recommencer"
-        print "> "
-        user_tentative = gets.chomp
+#méthode login
+def login (argument01, argument02)
+    puts "Bienvenue #{argument02}"
+    puts "Veuillez rentrez votre super mot de passe secretly secret"
+    print ">"
+    login = gets.chomp
+    if login == argument01
+    then puts "Bravo mon coco, te voilà rediriger vers notre incroyable et secrète page d'accueil..."
+    else puts "Mais NON, abruti, débile, incapable de se souvenir de son mot de passe!"
+        print ">"
+        login = gets.chomp
+
     end
+end
+
+#méthode welcome_screen
+def welcome_screen
+    puts "BIENVENUE .... dans ta zone ultra giga secrète...En parlant de ça, savais tu que Thibault est surnommé Le Magnifique ?"
+end
+
+#méthode perform
+def perform
+    array_sign_up = sign_up
+    var01 = array_sign_up[0]
+    var02 = array_sign_up[1]
+    print "Array récupérer via le return de la fct login#{array_sign_up}"
+    puts ""
+    login(var01, var02)
     welcome_screen
 end
 
-def welcome_screen 
-    puts "Bienvenue, pour envoyer une bombe nucléair, taper 1."
-end
-
-def perform 
-    user_mdp = signup
-    login (user_mdp)
-end
-
 perform
-    
-
---
+++ lib/03_stairway.rb
@@ -0,0 +1,9 @@
+def debut_partie
+    print "Bonjour, bienvenu dans ce jeu de marches claqué au sol."
+    print "Il va falloir tirer le dés et monter des marches imaginaires!. cool"
+    print "vous démarrez à la case 0. Et il faut monter à la case 10."
+end
+
+def lancer_de
+    return rand(1..6)
+end
